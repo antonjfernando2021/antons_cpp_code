6d3d4b4ce51e5967efc8102321cb6107a614a431
anton <antonjfernando@gmail.com>
2022-02-23 14:44:27 -0500

    how to use stringstream example

--- git_test.cpp
+++ git_test.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
@@ -13,4 +15,32 @@ int main(int argc, char* argv[])
    cout << "master:--> add a line from master to be rebased in fn1234" << endl;
    cout << "first PR2" << endl;
    cout << "first PR3" << endl;
+   std::vector<int> v = {1,2,3,4,5,6,7,8,9,10,11,12};
+   int i = 4;
+    for (auto x : v)
+    {
+	    i--;
+
+            cout << x << " ";
+	if(i==0)
+	{
+        cout << endl;
+	i=4;
+	}
+    }
+    cout << "try thist" << endl;
+    i = 4;
+    std::stringstream ss;
+    for (auto x : v)
+    {
+	    i--;
+
+            ss << x << " ";
+	if(i==0)
+	{
+        cout << ss.str() << endl;
+	i=4;
+	ss.str("");
+	}
+    }
 }
