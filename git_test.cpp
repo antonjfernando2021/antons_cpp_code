80d735d14b65d25f32d0e00f83272281b5376497
anton <antonjfernando@gmail.com>
2022-02-23 21:36:10 -0500

    adding the the pick line
    (cherry picked from commit f91f4699946673d38c5dcf2a8788181fe1ee92c5)

--- git_test.cpp
+++ git_test.cpp
@@ -11,4 +11,35 @@ int main(int argc, char* argv[])
    cout << "master:--> back inside master" << endl;
    cout << "master:--> FN_1234 inside again" << endl;
    cout << "master:--> add a line from master to be rebased in fn1234" << endl;
+   cout << "first PR2" << endl;
+   cout << "first PR3" << endl;
+   std::vector<int> v = {1,2,3,4,5,6,7,8,9,10,11,12};
+   int i = 4;
+    for (auto x : v)
+    {
+	    i--;
+
+            cout << x << " ";
+	if(i==0)
+	{
+        cout << endl;
+	i=4;
+	}
+    }
+    cout << "try thist" << endl;
+    i = 4;
+    std::stringstream ss;
+    for (auto x : v)
+    {
+	    i--;
+
+            ss << x << " ";
+	if(i==0)
+	{
+        cout << ss.str() << endl;
+	i=4;
+	ss.str("");
+	}
+    }
+    cout << "cherry pick this" << endl;
 }
