83c1a06fd0a7db13e7a28839349482b4a089222e
anton <antonjfernando@gmail.com>
2022-12-30 20:26:05 -0500

    ring buffer code

---
+++ buffer.cpp
@@ -0,0 +1,91 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <algorithm>
+#include <thread>
+#include <mutex>
+#include <future>
+#include <chrono>
+#include <future>
+#include <thread>         // std::this_thread::sleep_for
+#include <chrono>         // std::chrono::seconds
+#include <iostream>
+#include <thread>
+#include <chrono>
+using namespace std;
+
+class ringBuffer
+{
+    public:
+        ringBuffer() = default;
+        void Reader();
+        void Writer();
+        void startThreads();
+
+    private:
+        std::mutex mu;
+        std::condition_variable consumer_waiting_till_not_empty; // not empty means, consumer can take this 
+        std::condition_variable producer_waiting_till_not_full;  // not full, means producer can add things 
+        std::array<int,4> ring; 
+        bool is_empty{true};
+        bool is_full{false};
+        int  tail{0};
+        int  head{0};
+        int  number{100};
+};
+
+void ringBuffer::Writer() // consumer 
+{
+    std::unique_lock <std::mutex> mylock(mu, std::defer_lock);
+    ///std::cout << "Entering....consumer "<< std::endl;
+    while (1)
+    {
+        mylock.lock();
+        consumer_waiting_till_not_empty.wait( mylock, [this]() { return !is_empty; } ); // predicate which returns false if the waiting should be continued 
+        // consumer a buffer
+        printf("    Writer: consuming: ring[tail:%d] = %d\n", tail, ring.at(tail)) ; 
+        tail =  (tail + 1) % 4;
+        std::this_thread::sleep_for (std::chrono::seconds(5));
+        mylock.unlock();
+        producer_waiting_till_not_full.notify_one(); // not full, ask producer to produce more 
+    }
+}
+void ringBuffer::startThreads()
+{
+   std::thread tprod( [this] () { Reader(); } );
+   std::thread tcons( [this] () { Writer(); } );
+   tprod.join();
+   tcons.join();
+}
+
+void ringBuffer::Reader()  // producer 
+{
+    std::unique_lock <std::mutex> mylock(mu, std::defer_lock);
+    //std::cout << "Entering....producer "<< std::endl;
+    while (1)
+    {
+        mylock.lock();
+        producer_waiting_till_not_full.wait( mylock, [this]() { return !is_full; } ); // predicate which returns false if the waiting should be continued 
+        // add a new buffer
+        ring.at(head) = number++;
+        printf("Reader: producing: ring[head:%d] = %d\n", head, ring.at(head)) ; 
+        is_full = (head+1 == 4) ? true:false;
+        is_empty = ! is_full;
+        head = (head + 1) % 4;
+        std::this_thread::sleep_for (std::chrono::seconds(1));
+
+        mylock.unlock();
+        consumer_waiting_till_not_empty.notify_one(); // go consumer it, there is at least 1 in the ring buffer 
+    }
+}
+
+int main () 
+{
+    std::cout << "circular buffering ...." << std::endl;
+    ringBuffer rb;
+    //std::thread t1(ringBuffer::Reader, rb);
+    //std::thread t2(ringBuffer::Writer, rb);
+    rb.startThreads();
+    return 0;
+}
